4f132a3b67b43a3a4d57e114a7f257086d1fc8e4
Mamal Maleki <mohammadalimalekie@gmail.com>
2022-09-30 00:10:31 +0330

    schema definitions

---
+++ src/Catalog.Infrastructure/SchemaDefinitions/ItemEntitySchemaDefinition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catalog.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Catalog.Infrastructure.SchemaDefinitions
+{
+    public class ItemEntitySchemaDefinition :
+        IEntityTypeConfiguration<Item>
+    {
+        public void Configure(EntityTypeBuilder<Item> builder)
+        {
+            builder.Property(p => p.Price).HasConversion(
+                p => $"{p.Amount}:{p.Currency}",
+                p => new Money
+                {
+                    Amount = Convert.ToDecimal(p.Split(':', StringSplitOptions.None)[0]),
+                    Currency = p.Split(':', StringSplitOptions.None)[1]
+                }
+            );
+        }
+    }
+}
