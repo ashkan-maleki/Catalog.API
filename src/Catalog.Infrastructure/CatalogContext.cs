9cc9d44717c8358be3f0cd06162203bfe07bdb15
Mamal Maleki <mohammadalimalekie@gmail.com>
2022-09-29 18:39:34 +0330

    db context is created

---
+++ src/Catalog.Infrastructure/CatalogContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catalog.Domain.Entities;
+using Catalog.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Infrastructure
+{
+    public class CatalogContext : DbContext, IUnitOfWork
+    {
+        public const string DefaultSchema = "catalog";
+        public DbSet<Item>? Items { get; set; }
+
+        public CatalogContext(DbContextOptions<CatalogContext> options)
+            : base(options)
+        {
+
+        }
+
+        public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            await SaveChangesAsync(cancellationToken);
+            return true;
+        }
+    }
+}
