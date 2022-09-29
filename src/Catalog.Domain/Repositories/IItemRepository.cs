d2fba8cb2e52b6f351d2f2d1c699212bd613f8db
Mamal Maleki <mohammadalimalekie@gmail.com>
2022-09-29 18:38:21 +0330

    repository and unit of work patterns are implemented

---
+++ src/Catalog.Domain/Repositories/IItemRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catalog.Domain.Entities;
+
+namespace Catalog.Domain.Repositories
+{
+    public interface IItemRepository
+    {
+        Task<IEnumerable<Item>> GetAsync();
+        Task<Item> GetAsync(Guid id);
+        Item Add(Item item);
+        Item Update(Item item);
+    }
+}
---
+++ src/Catalog.Domain/Repositories/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Domain.Repositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task<int> SaveChangesAsync(
+            CancellationToken cancellationToken
+            = default(CancellationToken));
+
+        Task<bool> SaveEntitiesAsync(
+            CancellationToken cancellationToken
+                = default(CancellationToken));
+    }
+}
