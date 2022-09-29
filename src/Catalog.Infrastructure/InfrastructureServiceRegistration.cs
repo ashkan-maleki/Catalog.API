ba1ecec44767d184820822ed4299014934bfd67f
Mamal Maleki <mohammadalimalekie@gmail.com>
2022-09-30 00:28:54 +0330

    db context configuration

---
+++ src/Catalog.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Catalog.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<CatalogContext>(
+                options => options.UseSqlServer(
+                    configuration.GetConnectionString(
+                        "CatalogConnectionString"))
+                );
+            return services;
+        }
+    }
+}
