af8ee8fb348aeb32a9ed0b103ac47e9034c9c9bf
Mamal Maleki <mohammadalimalekie@gmail.com>
2022-10-05 18:28:33 +0330

    vailidation tests

---
+++ tests/Catalog.Domain.Tests/TestCases/AddItemRequestValidatorTests.cs
@@ -0,0 +1,34 @@
+﻿using Catalog.Domain.Entities;
+using Catalog.Domain.Requests.Item;
+using Catalog.Domain.Requests.Item.Validators;
+using FluentValidation.TestHelper;
+using Xunit;
+
+namespace Catalog.Domain.Tests.TestCases
+{
+    public class AddItemRequestValidatorTests
+    {
+        private readonly AddItemRequestValidator _validator;
+
+        public AddItemRequestValidatorTests()
+        {
+            _validator = new AddItemRequestValidator();
+        }
+
+        [Fact]
+        public void should_have_error_when_artist_id_is_null()
+        {
+            var addItemRequest = new AddItemRequest { Price = new Money() };
+            _validator.TestValidate(addItemRequest)
+                .ShouldHaveValidationErrorFor(x => x.ArtistId);
+        }
+
+        [Fact]
+        public void should_have_error_when_genre_id_is_null()
+        {
+            var addItemRequest = new AddItemRequest { Price = new Money() };
+            _validator.TestValidate(addItemRequest)
+                .ShouldHaveValidationErrorFor(x => x.GenreId);
+        }
+    }
+}
